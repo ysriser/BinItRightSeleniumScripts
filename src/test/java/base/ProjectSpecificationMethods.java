540bee0e3aa17c444220857c9d3a9a7a6f4fa6c3
Muthu-raj22 <e1539335@u.nus.edu>
2026-02-01 10:44:54 +0530

    Update Selenium SIT launch test and base setup

--- src/test/java/base/ProjectSpecificationMethods.java
+++ src/test/java/base/ProjectSpecificationMethods.java
@@ -1,5 +1,39 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class ProjectSpecificationMethods {
 
+	protected WebDriver driver;
+    protected String appUrl;
+
+    @BeforeMethod
+    public void setUp() {
+
+        // URL provided by CI/CD
+        appUrl = System.getenv("APP_URL");
+
+        if (appUrl == null) {
+            throw new RuntimeException("APP_URL not provided by CI/CD pipeline");
+        }
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
--- src/test/java/tests/LaunchTest.java
+++ src/test/java/tests/LaunchTest.java
@@ -1,12 +1,21 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import base.ProjectSpecificationMethods;
 
-public class LaunchTest {
+public class LaunchTest extends ProjectSpecificationMethods{
 
 	@Test
-    public void appShouldLaunch() {
-        // If app launches without crash → test passes
-        System.out.println("App launched successfully");
+    public void applicationShouldLaunch() {
+
+	        driver.get(appUrl);
+
+	        // If page loads without exception → PASS
+	        String title = driver.getTitle();
+
+	        Assert.assertNotNull(title, "Application loaded but page title is null");
+	        System.out.println("Application launched successfully at " + appUrl);
+	    
     }
 }
